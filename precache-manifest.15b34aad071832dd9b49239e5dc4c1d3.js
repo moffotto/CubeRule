800f17d3fe73ffcf064bb678c8bf11356b63a6cd
Sam Otto <moffotto@Sams-MacBook-Pro.local>
2019-02-05 02:18:42 -0800

    Updates

---
+++ precache-manifest.15b34aad071832dd9b49239e5dc4c1d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d10b67fa430344138a1",
+    "url": "/CubeRule/static/js/runtime~main.2d10b67f.js"
+  },
+  {
+    "revision": "4787c9cfc3e59c3f913b",
+    "url": "/CubeRule/static/js/main.4787c9cf.chunk.js"
+  },
+  {
+    "revision": "4928bccfdb80846973a2",
+    "url": "/CubeRule/static/js/1.4928bccf.chunk.js"
+  },
+  {
+    "revision": "4787c9cfc3e59c3f913b",
+    "url": "/CubeRule/static/css/main.3f98a56c.chunk.css"
+  },
+  {
+    "revision": "b5502783d8fe4cd209012a1fb4d28191",
+    "url": "/CubeRule/index.html"
+  }
+];
