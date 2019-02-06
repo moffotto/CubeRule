afab0cfe516acd157a5a569ab7590766ed4070c1
Sam Otto <moffotto@Sams-MacBook-Pro.local>
2019-02-05 16:38:21 -0800

    Updated page with more information

--- src/App/pages/Homepage/Homepage.js
+++ src/App/pages/Homepage/Homepage.js
@@ -3,9 +3,9 @@ import './Homepage.css';
 import { Showcase } from './components/Showcase';
 import { Image, Container, Row, Col } from 'react-bootstrap';
 
-const team = [{ name: "Adele Miller", bio: "This be Adele.", image: "./img/sam.jpg" }, { name: "Harshitha Akkaraju", bio: "This be Harshitha.", image: "./img/sam.jpg" }, { name: "Shaarika Kaul", bio: "This is Shaarika.", image: "./img/sam.jpg" }, { name: "Sam Otto", bio: "This be me.", image: "./img/sam.jpg" }];
+const team = [{ name: "Adele Miller", bio: "Adele is majoring in Informatics at the University of Washington. She is a long time hackathon organizer and has served as a teaching assistant in several courses in the Informatics program. She will be starting as a security engineer in September.", image: "./img/adele.jpg" }, { name: "Harshitha Akkaraju", bio: "This be Harshitha.", image: "./img/sam.jpg" }, { name: "Shaarika Kaul", bio: "This is Shaarika.", image: "./img/sam.jpg" }, { name: "Sam Otto", bio: "Sam is majoring in Informatics at the University of Washington with a dual focus in information security and data science. He will begin a position as a cyber risk consultant this upcoming summer.", image: "./img/sam.jpg" }];
 const awsImg = [{ src: "./img/aws_logo.svg" }, { src: "./img/aws_cloud.svg" }, { src: "./img/aws_ssl.svg" }];
-const sponsor = [{ name: "Joe Basirico", bio: "Security Innovation Joe.", image: "./img/sam.jpg" }]
+const sponsors = [{ name: "Joe Basirico", bio: "Joe is a Senior VP of Engineering at Security Innovation. Among other duties, he is responsible for the development of the CMD+CTRL product, which is the penetration testing platform that will support CubeRule.", image: "./img/security_innovation.png" }, { name: "Informatics, UW", bio: "This product is being developed within the context of the Informatics program capstone at the University of Washington, Seattle.", image: "./img/informatics_logo.png" }]
 
 export class Homepage extends Component {
 
@@ -25,7 +25,7 @@ export class Homepage extends Component {
                 </Container>
 
                 <Container>
-                    <Row className="row-margin-3" >
+                    <Row className="row-margin-4">
                         <h1 className="white-text">What this is</h1>
                     </Row>
                     <Row>
@@ -47,7 +47,7 @@ export class Homepage extends Component {
                 </Container>
 
                 <Container>
-                    <Row className="row-margin-3" >
+                    <Row className="row-margin-4" >
                         <h1 className="white-text">Who we are</h1>
                     </Row>
                     <Row className="row-margin-1" >
@@ -56,20 +56,32 @@ export class Homepage extends Component {
                 </Container>
 
                 <Container>
-                    <Row className="row-margin-3" >
-                        <h1 className="white-text">Our sponsor</h1>
+                    <Row className="row-margin-4" >
+                        <h1 className="white-text">Our sponsors</h1>
                     </Row>
                     <Row className="row-margin-1" >
-                        <Showcase group={sponsor} type="team" />
+                        <Showcase group={sponsors} type="team" />
                     </Row>
                 </Container>
 
                 <Container>
-                    <Row className="row-margin-3" >
-                        <h1 className="white-text">Contact us</h1>
-                    </Row>
-                    <Row className="row-margin-1 white-text" >
-                        If you are a developer or security engineer with AWS experience and would like to help test this application, or for any other reason, please feel free to contact us at: moffotto@uw.edu
+                    <Row className="row-margin-4">
+                        <Col xs={6}>
+                            <Row>
+                                <h1 className="white-text">Why "CubeRule"?</h1>
+                            </Row>
+                            <Row className="row-margin-1 white-text" >
+                                <p>This fictitious app must have a theme, and we derived it from the concept of the "Cube Rule", seen on <a className="white-text" href="http://cuberule.com/">cuberule.com.</a> The 'Cube Rule' specifies a method for classifying food into multiple categories, such as toast, taco, sandwhich, calzone, and quiche. The storyline for this application is that of a new startup offering the ability to detect these food classifications through the use of "computer vision techniques" and "smart AI technologies".</p>
+                            </Row>
+                        </Col>
+                        <Col xs={{ span: 5, offset: 1 }}>
+                            <Row>
+                                <h1 className="white-text">Contact us</h1>
+                            </Row>
+                            <Row className="row-margin-1 white-text" >
+                                If you are a developer or security engineer with AWS experience and would like to help test this application, or for any other reason, please feel free to contact us at: moffotto@uw.edu
+                        </Row>
+                        </Col>
                     </Row>
                 </Container>
 
